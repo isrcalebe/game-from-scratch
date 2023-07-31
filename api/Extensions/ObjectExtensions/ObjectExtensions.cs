060fb1903de001c28c2483ef4f896f688e1c4ca8
isrcalebe <morshu0@proton.me>
2023-07-31 11:06:55 -0300

    docs(extensions/objects): add xmldoc for `ObjectExtensions` class

--- api/Extensions/ObjectExtensions/ObjectExtensions.cs
+++ api/Extensions/ObjectExtensions/ObjectExtensions.cs
@@ -4,21 +4,44 @@
  * SPDX-License-Identifier: MIT
  */
 
+using System;
 using System.Diagnostics.CodeAnalysis;
 
 #nullable enable
 
 namespace miniblocks.API.Extensions.ObjectExtensions;
 
+/// <summary>
+/// A set of extension methods for <see cref="object"/>.
+/// </summary>
 public static class ObjectExtensions
 {
+    /// <summary>
+    /// Returns the non-null value of the target object, or throws a <see cref="NullReferenceException"/> if the target is null.
+    /// </summary>
+    /// <typeparam name="T">The type of the target object.</typeparam>
+    /// <param name="target">The target object.</param>
+    /// <returns>The non-null value of the target object.</returns>
+    /// <exception cref="NullReferenceException">Thrown when the target object is null.</exception>
     [return: NotNull]
     public static T AsNonNull<T>(this T? target)
-        => target!;
+        => target! ?? throw new NullReferenceException($"Expected \"{nameof(target)}\" to be non-null.");
 
+    /// <summary>
+    /// Determines whether the target object is null.
+    /// </summary>
+    /// <typeparam name="T">The type of the target object.</typeparam>
+    /// <param name="target">The target object.</param>
+    /// <returns>True if the target object is null, false otherwise.</returns>
     public static bool IsNull<T>([NotNullWhen(false)] this T target)
         => ReferenceEquals(target, null);
 
+    /// <summary>
+    /// Determines whether the target object is not null.
+    /// </summary>
+    /// <typeparam name="T">The type of the target object.</typeparam>
+    /// <param name="target">The target object.</param>
+    /// <returns>True if the target object is not null, false otherwise.</returns>
     public static bool IsNotNull<T>([NotNullWhen(true)] this T target)
         => !ReferenceEquals(target, null);
 }
